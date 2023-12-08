06f9e695f12723fdf5fa988985390dfa489ed989
James DiStefano <jamesdistefano@MacBook-Pro.local>
2023-12-08 12:41:02 -0500

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./button.css";
+import { Link } from "react-router-dom";
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+}) => {};
